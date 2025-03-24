ba3ee57a4883e855786a5ebb422a9c7975975c17
imashaRooster <imasha@rooster.org>
2025-03-24 15:28:31 +0530

    add new rule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('deployments')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('notify')
+  notifyDeployment(@Body() deploymentData: any) {
+    console.log('Deployment Notification Received:', deploymentData);
+    return {
+      message: 'Deployment data received successfully',
+      data: deploymentData,
+    };
   }
 }
